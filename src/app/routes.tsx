4625f1c13341820d8417a4381887dcabac924e89
imsangjun <152248126+imsangjun@users.noreply.github.com>
2026-04-15 23:22:36 +0900

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -6,6 +6,8 @@ import { Campaign } from "./components/Campaign";
 import { Login } from "./components/Login";
 import { Admin } from "./components/Admin";
 import { MyPage } from "./components/MyPage";
+import { Terms } from "./components/Terms";
+import { Privacy } from "./components/Privacy";
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +20,8 @@ export const router = createBrowserRouter([
       { path: "login", Component: Login },
       { path: "codeking1234!", Component: Admin },
       { path: "mypage", Component: MyPage },
+      { path: "terms", Component: Terms },
+      { path: "privacy", Component: Privacy },
     ],
   },
 ]);
